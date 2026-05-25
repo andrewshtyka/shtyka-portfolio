80c39de4ba663a1cb823825a00e7de94fcb5256d
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-05-25 19:43:25 +0300

    Add smooth scroll

--- src/components/DynamicCSS/DynamicCSS.tsx
+++ src/components/DynamicCSS/DynamicCSS.tsx
@@ -1,11 +1,29 @@
-import { HOME_SECTIONS_ARR_STYLING } from "@/constants/sectionNames";
+import {
+	HOME_SECTIONS,
+	HOME_SECTIONS_ARR_STYLING,
+} from "@/constants/sectionNames";
 
+/**
+ * Margin for home page sections with anchor links
+ */
 export default function DynamicCSS() {
-	// margin for home page sections with anchor links
-	// currently: Projects, Contact
-	const SELECTORS = HOME_SECTIONS_ARR_STYLING.map((id) => `#${id}`).join(", ");
+	// positive: Projects, Contact
+	const SELECTORS_OFFSET_POSITIVE = HOME_SECTIONS_ARR_STYLING.map(
+		(id) => `#${id}`
+	).join(", ");
 
-	const CSS = `${SELECTORS} { scroll-margin-top: var(--offset-sections-large); }`;
+	// negative: Experiments
+	const SELECTORS_OFFSET_NEGATIVE = `#${HOME_SECTIONS.experiments}`;
+
+	const CSS = `
+	${SELECTORS_OFFSET_POSITIVE} {
+		scroll-margin-top: var(--offset-sections-mid);
+	}
+
+	${SELECTORS_OFFSET_NEGATIVE} {
+		scroll-margin-top: calc(var(--offset-sections-small) * -1);
+	}
+	 `;
 
 	return <style dangerouslySetInnerHTML={{ __html: CSS }} />;
 }
--- src/lib/util/scrollToSection.ts
+++ src/lib/util/scrollToSection.ts
@@ -1,4 +1,7 @@
 export default function scrollToSection(id: string): void {
 	const element = document.getElementById(id);
-	if (element) element.scrollIntoView();
+	if (element)
+		element.scrollIntoView({
+			behavior: "smooth",
+		});
 }
