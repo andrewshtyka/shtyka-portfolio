93814546e1f1413f7b5a4fc519defe05f0add4a3
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-05-28 20:21:44 +0300

    Make project details a separate component

--- src/app/[lang]/_components/ProjectsSection/Card/Card.tsx
+++ src/app/[lang]/_components/ProjectsSection/Card/Card.tsx
@@ -10,7 +10,6 @@ import VideoProject from "@/components/VideoProject/VideoProject";
 import ButtonPrimary from "@/components/ButtonPrimary/ButtonPrimary";
 import IconArrowCurve from "@/components/Icons/IconArrowCurve/IconArrowCurve";
 import Divider from "@/components/Divider/Divider";
-import ListItem from "@/components/ListItem/ListItem";
 import IconArrowShortCut from "@/components/Icons/IconArrowShortCut/IconArrowShortCut";
 
 // constants
@@ -26,64 +25,27 @@ import css from "./Card.module.css";
 import { Props } from "./Card.types";
 
 // utils
-import processDetailsData from "./lib/helpers/processDetailsData";
 import { useParams } from "next/navigation";
 import React from "react";
+import ProjectDetails from "@/components/ProjectDetails/ProjectDetails";
 
 // #endregion ===========================
 
 export default function Card({ uiString, buttonTitle = "" }: Props) {
-	// animation - titles
-	const refTitle = React.useRef<HTMLHeadingElement>(null);
-	const refTitleCol_1 = React.useRef<HTMLHeadingElement>(null);
-	const refTitleCol_2 = React.useRef<HTMLHeadingElement>(null);
-	const refTitleCol_3 = React.useRef<HTMLHeadingElement>(null);
+	const params = useParams<{ lang: string }>();
+	const lang = params?.lang ?? "en";
+	const ui = JSON.parse(uiString);
 
+	// animation - title
+	const refTitle = React.useRef<HTMLHeadingElement>(null);
 	const isTitleInView = useInView(refTitle, {
 		once: true,
 		margin: "-24px 0px -24px 0px",
 	});
-	const isTitleCol_1_InView = useInView(refTitleCol_1, {
-		once: true,
-		margin: "-24px 0px -24px 0px",
-	});
-	const isTitleCol_2_InView = useInView(refTitleCol_2, {
-		once: true,
-		margin: "-24px 0px -24px 0px",
-	});
-	const isTitleCol_3_InView = useInView(refTitleCol_3, {
-		once: true,
-		margin: "-24px 0px -24px 0px",
-	});
 	const { play: playTitle } = useLinkHover(refTitle);
-	const { play: playTitleCol_1 } = useLinkHover(refTitleCol_1);
-	const { play: playTitleCol_2 } = useLinkHover(refTitleCol_2);
-	const { play: playTitleCol_3 } = useLinkHover(refTitleCol_3);
-
 	React.useEffect(() => {
 		if (isTitleInView) playTitle?.();
 	}, [isTitleInView, playTitle]);
-
-	React.useEffect(() => {
-		if (isTitleCol_1_InView) playTitleCol_1?.();
-	}, [isTitleCol_1_InView, playTitleCol_1]);
-
-	React.useEffect(() => {
-		if (isTitleCol_2_InView) playTitleCol_2?.();
-	}, [isTitleCol_2_InView, playTitleCol_2]);
-
-	React.useEffect(() => {
-		if (isTitleCol_3_InView) playTitleCol_3?.();
-	}, [isTitleCol_3_InView, playTitleCol_3]);
-
-	const params = useParams<{ lang: string }>();
-	const lang = params?.lang ?? "en";
-
-	const ui = JSON.parse(uiString);
-
-	const details_1 = processDetailsData(ui?.details[0]?.content);
-	const details_2 = processDetailsData(ui?.details[1]?.content);
-	const details_3 = processDetailsData(ui?.details[2]?.content);
 
 	return (
 		<li className={css.container}>
@@ -145,107 +107,7 @@ export default function Card({ uiString, buttonTitle = "" }: Props) {
 				</motion.div>
 
 				{/* details */}
-				<div className={css.bottom}>
-					{/* col 1 */}
-					<motion.div className={`${css.part_1} ${css.grid}`}>
-						<motion.h5
-							ref={refTitleCol_1}
-							className={`${css.column_title} f_mono`}
-							variants={SECTION_PROJECTS_ANIMATION.title}
-							initial="initial"
-							whileInView="animate"
-							viewport={SECTION_PROJECTS_ANIMATION.title.viewport}
-						>
-							{details_1?.title}
-						</motion.h5>
-						<motion.ul
-							className={css.list}
-							variants={SECTION_PROJECTS_ANIMATION.project.list}
-							initial="hidden"
-							whileInView="visible"
-							viewport={SECTION_PROJECTS_ANIMATION.project.list.viewport}
-						>
-							{details_1?.items?.map(({ key, item }) => (
-								<ListItem
-									key={key}
-									variants={SECTION_PROJECTS_ANIMATION.project.listItem}
-									viewport={
-										SECTION_PROJECTS_ANIMATION.project.listItem.viewport
-									}
-								>
-									{item}
-								</ListItem>
-							))}
-						</motion.ul>
-					</motion.div>
-
-					{/* col 2 */}
-					<motion.div className={`${css.part_2} ${css.grid}`}>
-						<motion.h5
-							ref={refTitleCol_2}
-							className={`${css.column_title} f_mono`}
-							variants={SECTION_PROJECTS_ANIMATION.title}
-							initial="initial"
-							whileInView="animate"
-							viewport={SECTION_PROJECTS_ANIMATION.title.viewport}
-						>
-							{details_2?.title}
-						</motion.h5>
-						<motion.ul
-							className={css.list}
-							variants={SECTION_PROJECTS_ANIMATION.project.list}
-							initial="hidden"
-							whileInView="visible"
-							viewport={SECTION_PROJECTS_ANIMATION.project.list.viewport}
-						>
-							{details_2?.items?.map(({ key, item }) => (
-								<ListItem
-									key={key}
-									variants={SECTION_PROJECTS_ANIMATION.project.listItem}
-									viewport={
-										SECTION_PROJECTS_ANIMATION.project.listItem.viewport
-									}
-								>
-									{item}
-								</ListItem>
-							))}
-						</motion.ul>
-					</motion.div>
-
-					{/* col 3 */}
-					<motion.div className={`${css.part_3} ${css.right} ${css.grid}`}>
-						<motion.h5 className={`${css.column_title} f_mono`}>
-							<motion.span
-								ref={refTitleCol_3}
-								variants={SECTION_PROJECTS_ANIMATION.title}
-								initial="initial"
-								whileInView="animate"
-								viewport={SECTION_PROJECTS_ANIMATION.title.viewport}
-							>
-								{details_3?.title}
-							</motion.span>
-						</motion.h5>
-						<motion.ul
-							className={css.list}
-							variants={SECTION_PROJECTS_ANIMATION.project.list}
-							initial="hidden"
-							whileInView="visible"
-							viewport={SECTION_PROJECTS_ANIMATION.project.list.viewport}
-						>
-							{details_3?.items?.map(({ key, item }) => (
-								<ListItem
-									key={key}
-									variants={SECTION_PROJECTS_ANIMATION.project.listItem}
-									viewport={
-										SECTION_PROJECTS_ANIMATION.project.listItem.viewport
-									}
-								>
-									{item}
-								</ListItem>
-							))}
-						</motion.ul>
-					</motion.div>
-				</div>
+				<ProjectDetails uiString={uiString} />
 
 				{/* button */}
 				<motion.div
---
+++ src/components/ProjectDetails/ProjectDetails.types.ts
@@ -0,0 +1,4 @@
+export interface Props {
+	uiString: string;
+	offset?: "small" | "large";
+}
