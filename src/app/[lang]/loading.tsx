1974007f6e8f72c312f13c05a88925c2f0e82895
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-06-02 10:57:04 +0300

    Test loading screen - flash

---
+++ src/app/[lang]/loading.tsx
@@ -0,0 +1,11 @@
+export default async function Loading() {
+	return (
+		<div
+			style={{
+				backgroundColor: "var(--color-gray)",
+				width: "100svw",
+				height: "100dvh",
+			}}
+		></div>
+	);
+}
