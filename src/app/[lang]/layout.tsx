75b0e3f4a404c15e794f41a9cc5a080b5bf98660
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-06-27 22:34:32 +0300

    fix opengraph

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -81,7 +81,7 @@ export async function generateMetadata() {
 
 		openGraph: {
 			siteName: titleProfile,
-			images: [{ url: "../opengraph.webp", width: 1200, height: 630 }],
+			images: [{ url: "/opengraph.webp", width: 1200, height: 630 }],
 			locale: isUa ? "uk_UA" : "en_US",
 			alternateLocale: isUa ? "en_US" : "uk_UA",
 			type: "website",
@@ -89,7 +89,7 @@ export async function generateMetadata() {
 
 		twitter: {
 			card: "summary_large_image",
-			images: ["../opengraph.webp"],
+			images: ["/opengraph.webp"],
 		},
 
 		robots: {
